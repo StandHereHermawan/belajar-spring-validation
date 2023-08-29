02e6928cbbc8cae1195490dd7548f278612de85d
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-29 20:41:37 +0700

    Initial commit

---
+++ src/main/java/ariefbelajarteknologi/belajarspringvalidation/BelajarSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.belajarspringvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringValidationApplication.class, args);
+	}
+
+}
