d2b653b285cda19bf776aad511415f019aa2ce07
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-31 10:32:35 +0700

    Method Validation, add helper interfaces

---
+++ src/main/java/ariefbelajarteknologi/belajarspringvalidation/helper/ISayHello.java
@@ -0,0 +1,11 @@
+package ariefbelajarteknologi.belajarspringvalidation.helper;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public interface ISayHello {
+
+    String sayHello(@NotBlank String name);
+
+}
