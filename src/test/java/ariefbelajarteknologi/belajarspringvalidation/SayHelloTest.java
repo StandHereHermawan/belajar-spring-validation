79e3e5d2085cfb2bb10f34d667c700426359be22
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-31 10:23:20 +0700

    Method Validation, add unit test

---
+++ src/test/java/ariefbelajarteknologi/belajarspringvalidation/SayHelloTest.java
@@ -0,0 +1,28 @@
+package ariefbelajarteknologi.belajarspringvalidation;
+
+import ariefbelajarteknologi.belajarspringvalidation.helper.SayHello;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SayHelloTest {
+
+    @Autowired
+    private SayHello sayHello;
+
+    @Test
+    void testSuccess() {
+        String message = sayHello.sayHello("Arief");
+        Assertions.assertEquals("Hello Arief", message);
+    }
+
+    @Test
+    void testError() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            sayHello.sayHello("");
+        });
+    }
+}
