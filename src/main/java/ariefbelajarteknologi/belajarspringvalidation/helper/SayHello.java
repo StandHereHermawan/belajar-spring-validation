22631852476f0a964de5e81164bcd8aaaa4ba5d3
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-31 10:23:00 +0700

    Method Validation, add helper class

---
+++ src/main/java/ariefbelajarteknologi/belajarspringvalidation/helper/SayHello.java
@@ -0,0 +1,14 @@
+package ariefbelajarteknologi.belajarspringvalidation.helper;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@Validated
+public class SayHello {
+
+    public String sayHello(@NotBlank String name) {
+        return "Hello " + name;
+    }
+}
